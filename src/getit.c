fb65abbb934beb20814466348c4274b031a7da97
AIBaxt <andrewbmi2@gmail.com>
2019-10-07 00:42:50 -0400

    Getit up and running

--- src/getit.c
+++ src/getit.c
@@ -1,8 +1,15 @@
 #include "getit.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 int getit(char * line, int nline){
-
-return 0;
+    char * token = strtok(line, " ");
+    double qNums[3];
+    int i = 0;
+    while(token !=NULL){
+        qNums[i] = atof(token);
+        token = strtok(NULL, " ");
+        i++;
+    }
 }
--- src/qs.c
+++ src/qs.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "getit.h"
 
+void input();
 
-int main(int argc, char* argv[]){
-    
+int main(int argc, char *argv[]){
+    input();
+}
+
+void input(){
+    char * buf = malloc(sizeof (char*) * 20);
+    printf("Enter the values for a, b, and c:\n");
+    fgets(buf, 20, stdin);
+  //  scanf("%s", buf);
+    getit(buf, strlen(buf));   
 }
