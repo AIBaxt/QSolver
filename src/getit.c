85a90698bc26907079ffbe94498d9ee8b7d323ac
Adam Johnson <adam.l.johnson@wmich.edu>
2019-10-08 21:59:29 -0400

    changed getit to fit closer to what he wants this function to do, but apparently std=c11 doesn't contain the strnlen he wanted?

--- src/getit.c
+++ src/getit.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int getit(char * line, int nline){
-    char * token = strtok(line, " ");
+    /*char * token = strtok(line, " ");
     double qNums[3];
     int i = 0;
     while(token !=NULL){
@@ -12,4 +12,25 @@ int getit(char * line, int nline){
         token = strtok(NULL, " ");
         i++;
     }
+	*/
+
+
+	int error = 0; 
+	int size = 0;
+
+	line = (char *) malloc(nline * sizeof(char));
+
+	if (fgets(line, nline, stdin) == NULL){
+		error = 1; // no input, just an empty line
+	}
+	else 
+	{
+		size = strnlen(line, nline);
+		if (size == nline && line[nline-1] != '\0') {
+			error = 2; // didn't read the whole line, got cut off, so no null terminator
+		}
+	
+	} 
+	
+	return error;
 }
