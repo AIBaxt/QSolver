1798e1304e6392fab7025e17b09e786c7d95e7c2
AIBaxt <andrewbmi2@gmail.com>
2019-10-14 02:13:10 -0400

    Misc changes, a few comments.

--- src/qs.c
+++ src/qs.c
@@ -30,5 +30,5 @@ int main(int argc, char *argv[]){
 
 
 void displayHelp(){
-
+//Here we would display example input and output to the user.
 }
--- src/validate.c
+++ src/validate.c
@@ -3,11 +3,8 @@
 #include <string.h>
 
 int validate(char * line, int nline, double * a, double * b, double * c){
- 
 	int error = 0;
-	//char checkForQ;
-	//char checkForHelp[100];
-
+	//A toupper function would be nice here
 	if(strncmp(line, "q", 1) == 0 || strncmp(line, "Q", 1) == 0){
 		error = 2; //User is requesting to exit
 	}
@@ -19,27 +16,4 @@ int validate(char * line, int nline, double * a, double * b, double * c){
 	}
 
 	return error;
-	
-/*	if (sscanf(line, "%lf %lf %lf", a, b, c) != 3) // did not receive 3 numbers
-		error = 1;
-	
-	// if there is a character in the first place, check it for 'q' for quit
-	if (sscanf(line, "%c", checkForQ) == 1) 
-	{
-		if (checkForQ == 'Q' || checkForQ == 'q')
-			error = 2; // not technically an error, but easiest way to main to quit
-	}
-	
-	// if there is a string, check it for 'help'
-	if (sscanf(line, "%s", &checkForHelp) == 1)
-	{
-		if (strcasecmp(checkForHelp, "help") == 0)
-			error = 3;  // again, easiest way
-	}
-*/
-	
-	
-
-	// error = 0 on all 3 numbers received, = 1 if trash line, = 2 on quit, = 3 on help
-	//return error;   
 }
