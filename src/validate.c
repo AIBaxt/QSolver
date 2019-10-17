bc5414d1b3c6e15b9c0bd9dc05b4a0f625263d75
Adam Johnson <adam.l.johnson@wmich.edu>
2019-10-17 17:35:43 -0400

    Updated validate to check IEEE standards

--- src/validate.c
+++ src/validate.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include <string.h>
+#include <float.h>
+#include <math.h>
 
 int validate(char * line, int nline, double * a, double * b, double * c){
 	int error = 0;
@@ -15,5 +17,32 @@ int validate(char * line, int nline, double * a, double * b, double * c){
 		error = 1; // Either no input or did not receive all 3 numbers
 	}
 
+
+	if (error == 0) // I have all three numbers, no issues so far
+	{
+		if (*a > FLT_MAX || *a < FLT_MIN || *b > FLT_MAX || *b < FLT_MIN || *c > FLT_MAX || *c < FLT_MIN)
+			error = 4; // number not within FLT range
+
+		if (isinf(*a) || isinf(*b) || isinf(*c))
+			error = 5; // number is infinite
+
+		if (isnan(*a) || isnan(*b) || isnan(*c))
+			error = 6; // number is nan
+
+		if (!(isnormal(*a) && isnormal(*b) && isnormal(*c)))
+			error = 7; // number is subnormal/denormal
+
+		if (*a == 0)
+			error = 8; // a is 0, not a legitimite quadratic formula
+
+	}
+
+	
+
+/* ERROR CODE DESCRIPTION:
+1: Incorrect input
+2,3: User Request
+4-8: Error in numbers (specifications in code)
+*/
 	return error;
 }
