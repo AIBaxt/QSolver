2901728c1db8e1fabb6fc2bbec80525a72fa627a
Adam Johnson <adam.l.johnson@wmich.edu>
2019-10-17 19:38:17 -0400

    main was edited to include other files

--- src/qs.c
+++ src/qs.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include "getit.h"
 #include "validate.h"
+#include "qsolve.h"
+#include "initialDisplay.h"
+#include "loopDisplay.h"
+#include "endDisplay.h"
 
 void displayHelp();
 
@@ -11,19 +15,39 @@ int main(int argc, char *argv[]){
     int size = 20; //size isn't definite, not entirely sure what to set it to
     char *line = malloc (sizeof (char*) * size);
     double a, b, c;
-    int errFlag = 0; // 0 for all clear, 1 for no input, 2 for exit, 3 for help menu
+    double x1, x2;
+    int errFlag1 = 0; // 0 for all clear, 1 for no input, 2 for exit, 3 for help menu
+    int errFlag2 = 0;
 
-    printf("Quadratic Equation Solver\n");
-    printf("Q to quit, H for help, or enter your three values.\n");
-    
-    while(errFlag!=2)
+    initialDisplay();
+
+    while(errFlag1 !=2)
     {
-        printf("Input: ");
+        loopDisplay();
+
         getit(line, size);
-        errFlag = (validate(line, size, &a, &b, &c));
-        if(errFlag == 3)
+
+        errFlag1 = (validate(line, size, &a, &b, &c));
+
+        if(errFlag1 == 3)
         {
            displayHelp();
+        }
+        else if (errFlag1 >= 4)
+        {
+            printf("ERROR: A number was either INF, NAN, Subnormal, beyond float, or A was 0");
+        }
+        else if (errFlag1 == 0)
+        {
+            errFlag2 = qsolve(a,b,c,&x1,&x2);
+
+            if (errFlag2 == 1)
+                printf("Roots undefined: complex number");
+            else
+            {
+                endDisplay(x1,x2);
+            }
+            
         }
     }
 }
