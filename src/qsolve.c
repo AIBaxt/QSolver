179f9447e4a48d7a6d25300def6839dbfb28d5b0
Adam Johnson <adam.l.johnson@wmich.edu>
2019-10-17 18:38:32 -0400

    qsolve function has been added and filled in

--- src/qsolve.c
+++ src/qsolve.c
@@ -1,8 +1,30 @@
+#include <math.h>
+
 int qsolve(double a, double b, double c, double * x1, double * x2)
 {
     int error = 0;
+    double possibleTemp = 0;
 
+    double insideSQRT = pow(b, 2) - (4*a*c);
+
+    if (insideSQRT < 0) 
+        error = 1; // number is negative, sqrt(negative) is complex
+    else
+    {
+        *x1 = ((-1)*b + sqrt(insideSQRT)) / (2*a);
+        *x2 = ((-1)*b - sqrt(insideSQRT)) / (2*a);
+
+        if (x1 > x2)
+        {
+            possibleTemp = *x1;
+            *x1 = *x2;
+            *x2 = possibleTemp;
+        }
+    }
     
+    
+//ERROR DESCRIPTION
+//1: Complex Number Due to SQRT of negative 
 
     return error; 
 }
